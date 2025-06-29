b89f4aa68806e89f813ff7cc4a594ee0bf8d9b02
Charlie Steenhagen <csteen1005@gmail.com>
2025-06-29 08:30:58 -0500

    Added analytics

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom"
 import { ReactFlowProvider } from "@xyflow/react"
 import { SessionProvider } from "./context/SessionContext"
 import { KeyboardProvider } from "./context/KeyboardContext.tsx"
+import { Analytics } from "@vercel/analytics/react"
 
 const Providers = () => {
   return (
     <SessionProvider>
       <KeyboardProvider>
         <ReactFlowProvider>
+          <Analytics />
           <Outlet />
         </ReactFlowProvider>
       </KeyboardProvider>
